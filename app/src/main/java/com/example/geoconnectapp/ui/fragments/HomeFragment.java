ac51704bb343438f80613989e919dbafcf37f3a3
Rawbun34 <113198802+Rawbun34@users.noreply.github.com>
2024-03-17 18:01:50 +0100

    add hard-coded user to home fragment page

--- app/src/main/java/com/example/geoconnectapp/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/example/geoconnectapp/ui/fragments/HomeFragment.java
@@ -3,6 +3,8 @@ package com.example.geoconnectapp.ui.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -63,8 +65,9 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return rootView;
     }
 
 
