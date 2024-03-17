e3d8d9144b27312ffe009b003b90ddc25f03f4ef
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2024-03-17 15:41:56 +0100

    Added friend to friends tab

--- app/src/main/java/com/example/geoconnectapp/FriendFragment.java
+++ app/src/main/java/com/example/geoconnectapp/FriendFragment.java
@@ -1,33 +1,31 @@
-package com.example.geoconnectapp.ui.fragments;
+package com.example.geoconnectapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.geoconnectapp.FriendFragment;
-import com.example.geoconnectapp.R;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FriendsFragment#newInstance} factory method to
+ * Use the {@link FriendFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FriendsFragment extends Fragment {
+public class FriendFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public FriendsFragment() {
+    public FriendFragment() {
         // Required empty public constructor
     }
 
@@ -37,11 +35,10 @@ public class FriendsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FriendsFragment.
+     * @return A new instance of fragment FriendFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static FriendsFragment newInstance(String param1, String param2) {
-        FriendsFragment fragment = new FriendsFragment();
+    public static FriendFragment newInstance(String param1, String param2) {
+        FriendFragment fragment = new FriendFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,11 +59,20 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        getParentFragmentManager().beginTransaction()
-                .add(R.id.friendsList, new FriendFragment(), "friend_fragment_")
-                .commit();
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        return inflater.inflate(R.layout.fragment_friend, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ImageView message = view.findViewById(R.id.messageImage);
+
+        message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: go to message window
+            }
+        });
     }
 }
