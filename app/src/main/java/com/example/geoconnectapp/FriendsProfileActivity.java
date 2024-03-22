36d6b5f03522d69d317d5d57411f51fa9301a8f0
Quinnca77 <50358541+Quinnca77@users.noreply.github.com>
2024-03-22 22:05:51 +0100

    Implemented distance view in friendprofileactivity page

--- app/src/main/java/com/example/geoconnectapp/FriendsProfileActivity.java
+++ app/src/main/java/com/example/geoconnectapp/FriendsProfileActivity.java
@@ -1,11 +1,15 @@
 package com.example.geoconnectapp;
 
+import static java.lang.Math.floor;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.FragmentManager;
@@ -13,12 +17,19 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.geoconnectapp.logic.Tracking;
 import com.example.geoconnectapp.ui.fragments.LocationFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class FriendsProfileActivity extends AppCompatActivity {
     private ImageView arrowBack;
     private TextView distanceView;
     private AppCompatButton aloneBtn;
     private MainActivity parentActivity;
+    private FirebaseFirestore db;
+    private Tracking trackingHandler;
 //    final double userLat[] = {parentActivity.getUserLat()};
 //    final double userLong[] = {parentActivity.getUserLong()};
     @Override
@@ -26,28 +37,20 @@ public class FriendsProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_profile);
 
+        this.db = FirebaseFirestore.getInstance();
+
         // Register image view arrow back
         arrowBack = findViewById(R.id.arrowback);
 
         // Navigate back to the last page the user visit
         arrowBack.setOnClickListener(v -> finish());
 
-//        // The text view that displays the distance from yourself to the geocache
-//        distanceView = findViewById(R.id.profilePageDistance);
-//
+        // The text view that displays the distance from yourself to the geocache
+        distanceView = findViewById(R.id.profilePageDistance);
+
         // Create an instance of MainActivity class
         parentActivity = new MainActivity();
-//
-//        // Calling getTrackingHandler() method from MainActivity
-//        Tracking trackingHandler = parentActivity.getTrackingHandler();
-//
-//        // Latitude and longitude of geocache
-//        userLat[0] = parentActivity.getUserLat();
-//        userLong[0] = parentActivity.getUserLong();
-//
-//        // Change the text view to the corresponding distance
-//        distanceView.setText(String.format("%.2f m", trackingHandler.calculateDistance(userLat[0], userLong[0])));
-//
+
         // Register find geocache alone button
         aloneBtn = findViewById(R.id.alone);
 
@@ -58,5 +61,7 @@ public class FriendsProfileActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        // TODO: ProfilePageDistance update
+        distanceView.setText(getIntent().getStringExtra("distance"));
     }
 }
